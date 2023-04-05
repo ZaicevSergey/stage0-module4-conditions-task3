6d7a5de7c612280dfa971c116999d651c64ea364
ZaycevSergey <77642306+ZaicevSergey@users.noreply.github.com>
2023-04-05 11:15:45 +0300

    solved

--- src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task3/DaysInMonthPrinter.java
@@ -2,5 +2,45 @@ package school.mjc.stage0.conditions.task3;
 
 public class DaysInMonthPrinter {
     public void amountOfDays(int month) {
+        switch (month) {
+            case 1:
+                System.out.println("31 days");
+                break;
+            case 2:
+                System.out.println("28");
+                break;
+            case 3:
+                System.out.println("31 days");
+                break;
+            case 4:
+                System.out.println("30 days");
+                break;
+            case 5:
+                System.out.println("31 days");
+                break;
+            case 6:
+                System.out.println("30");
+                break;
+            case 7:
+                System.out.println("31");
+                break;
+            case 8:
+                System.out.println("31 days");
+                break;
+            case 9:
+                System.out.println("30 days");
+                break;
+            case 10:
+                System.out.println("31 days");
+                break;
+            case 11:
+                System.out.println("30 days");
+                break;
+            case 12:
+                System.out.println("31 days");
+                break;
+            default:
+                System.out.println("wrong number!");
+        }
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
+++ src/main/java/school/mjc/stage0/conditions/task3/SalaryCalculator.java
@@ -2,5 +2,19 @@ package school.mjc.stage0.conditions.task3;
 
 public class SalaryCalculator {
     public void calculateSalary(int salary) {
+        double taxPercentage;
+        if (salary <= 0) {
+            System.out.println("wrong input!");
+            return;
+        } else if (salary <= 10000) {
+            taxPercentage = 0.15;
+        } else if (salary <= 20000) {
+            taxPercentage = 0.18;
+        } else {
+            taxPercentage = 0.2;
+        }
+        double taxAmount = taxPercentage * salary;
+        double netSalary = salary - taxAmount;
+        System.out.println(netSalary);
     }
 }
